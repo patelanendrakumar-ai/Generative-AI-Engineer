e59b07e8252c061354e0674ef5a4e39eb7e45bee
Anendra Patel <patel.anendra.kumar@gmail.com>
2026-07-14 03:27:20 +0530

    Completed Chapter 4 - Variables and Data Types

---
+++ 04_Python_Variables_Data_Types/assignment.md
@@ -0,0 +1,8 @@
+Variable क्या होता है?
+Data Type क्या होता है?
+int और float में अंतर।
+String क्या है?
+Boolean क्या है?
+input() क्या Return करता है?
+type() Function क्या करता है?
+int() का उपयोग क्यों करते हैं?
---
+++ 04_Python_Variables_Data_Types/practice.py
@@ -0,0 +1,16 @@
+name=input("Enter Your Name:")
+print("Hello", name)
+
+age=int(input("Enter Your Age:"))
+print("Your Age is: ", age)
+
+city=input("Enter Your City:")
+print("You Live in: ", city)
+
+name=input("Name: ")
+age=int(input("Age:"))
+city=input("City:")
+print("------------- Student Details-------------")
+print("Name: ", name)
+print("Age: ", age)
+print("City: ", city)
