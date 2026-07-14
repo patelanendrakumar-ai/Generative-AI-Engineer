40830e42cb3809bff2217892c4c00f5dcfa3fb60
Anendra Patel <patel.anendra.kumar@gmail.com>
2026-07-15 00:12:36 +0530

    Completed Chapter 4 - Variables and Data Types

---
+++ 04_Python_Variables_Data_Types/mentor_challange_2.py
@@ -0,0 +1,21 @@
+"""
+Homework: 
+
+अपने Program में दो नई Fields जोड़िए:
+
+Email
+Dream AI Project
+
+Output ऐसा होना चाहिए:
+
+========== PROFILE ==========
+
+Name              : Anendra
+Age               : 25
+City              : Sidhi
+Email             : example@email.com
+Favorite Language : Python
+Dream AI Project  : AI Coding Assistant
+
+=============================
+"""
